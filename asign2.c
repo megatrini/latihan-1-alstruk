3b4cb9d87b25308028e4b9f631bd5db90ab2dfdd
Mega Trini Oktaviani <62134888+megatrini@users.noreply.github.com>
2020-03-13 16:10:26 +0700

    Create asign2.c

---
+++ asign2.c
@@ -0,0 +1,19 @@
+/* fILe : asign2.c */
+/* Deskripsi :
+/* Program ini berisi contoh sederhana untuk mendefinisikan variabel-variabel
+bilangan bulat (short int, int, long int), karakter, bilangan riil, */
+#include <stdio.h>
+int main ()
+{/* kamus */
+
+int u = 18;
+float t = 156.333;
+float b = 48.533;
+char c = 'P';
+/*inisialisasi karakter dengan karakter */
+printf ("Jenis kelamin = %c\n",c);
+printf ("tinggi = %.1f cm \n",t);
+printf ("berat= %.3f kg \n",b);
+printf ("Usia = %d tahun \n ", u);
+
+}
